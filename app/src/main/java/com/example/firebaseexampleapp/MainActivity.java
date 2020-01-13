3809e71f2027f1328cda1ba8604f549fc362463d
tewari6850 <tewarisuyash@gmail.com>
2020-01-13 13:12:48 -0600

    fixed

--- app/src/main/java/com/example/firebaseexampleapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseexampleapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.firebaseexampleapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,7 +13,12 @@ import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +28,14 @@ public class MainActivity extends AppCompatActivity {
     private int dateDay;
     private int dateYear;
 
+    FirebaseDatabaseHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new FirebaseDatabaseHelper();
 
         //  Video to learn basic access to CalendarView Data
         //  https://www.youtube.com/watch?v=WNBE_3ZizaA
@@ -59,6 +70,9 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
            Log.i(TAG, "Trying to add: " + eventName + ", " + dateSelected);
+           Event newEvent = new Event(eventName, dateSelected, dateYear, dateMonth, dateDay);
+           eventNameET.setText("");
+           dbHelper.addEvent(newEvent);
         }
     }
 
@@ -75,5 +89,43 @@ public class MainActivity extends AppCompatActivity {
             InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
+    }
+
+    // add this method to the end
+// call this method in the onClick for the show data method in the activity_main.xml show events button
+
+
+    public void onRetrieve(View v){
+        dbHelper.getDatabaseReference().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ArrayList<Event> currentEvents = new ArrayList<Event>();
+
+                for (DataSnapshot item : dataSnapshot.getChildren())
+                {
+                    Event e = new Event(
+                            item.child("eventName").getValue().toString(),
+                            item.child("eventDate").getValue().toString(),
+                            Integer.valueOf(item.child("year").getValue().toString()),
+                            Integer.valueOf(item.child("month").getValue().toString()),
+                            Integer.valueOf(item.child("day").getValue().toString()),
+                            item.child("key").getValue().toString());
+                    currentEvents.add(e);
+                }
+
+                // starts intent that will display this new data that has been saved into the arraylist
+                // since we used a single value event the data will not continually update
+
+                Intent intent = new Intent(MainActivity.this, DisplayEventsActivity.class);
+                intent.putExtra("events", currentEvents);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.i("callError", "There has been an Error with database retrieval");
+            }
+        });
+
     }
 }
